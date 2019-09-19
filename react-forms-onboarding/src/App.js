fa483828c49119a3ba593a2f2405bb911778ef1a
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-19 14:43:09 +0100

    add Formik to form

--- react-forms-onboarding/src/App.js
+++ react-forms-onboarding/src/App.js
@@ -1,28 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Form from './Form';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <Form />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const initalUserForm = {
+  name: "",
+  email: "",
+  password: ""
+};
+
+const [userList, setUserList] = useState([]);
 
 export default App;
--- react-forms-onboarding/src/Form.js
+++ react-forms-onboarding/src/Form.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import { Form } from 'formik';
+import React from "react";
+import { Form, Formik, Field, ErrorMessage } from "formik";
 
-
-<Form >
-<div>
-    <label>
-        Name
-        <Field name='name'
-        type='text'
-        placeholder='Name' />
-
-    </label>
-    <label>
-    <Field name='email'
-        type='text'
-        placeholder='Email' />
-
-    </label>
-    <label>
-    <Field name='password'
-        type='text'
-        placeholder='Password' />
-    </label>
-    <input type='checkbox'
-    /> Terms of Service
-</div>
-<button type='submit'>Submit</button>
-</Form>
+function userForm() {
+  return (
+    <Formik
+      initialValues={initialUserForm}
+      onSubmit={onSubmit}
+      render={props => {
+        return (
+          <Form>
+            <div>
+              <label>
+                Name
+                <Field name="name" type="text" placeholder="Name" />
+              </label>
+              <label>
+                <Field name="email" type="text" placeholder="Email" />
+              </label>
+              <label>
+                <Field name="password" type="text" placeholder="Password" />
+              </label>
+              <input type="checkbox" /> Terms of Service
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        );
+      }}
+    />
+  );
+}
 
 export default Form;
