951d32dfc7a01424ff4596b0384558bde403f3bf
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-19 14:01:01 +0100

    create initial bare bones of form

--- react-forms-onboarding/src/Form.js
+++ react-forms-onboarding/src/Form.js
@@ -1,4 +1,31 @@
 import React from 'react';
 import { Form } from 'formik';
 
+
+<Form >
+<div>
+    <label>
+        Name
+        <Field name='name'
+        type='text'
+        placeholder='Name' />
+
+    </label>
+    <label>
+    <Field name='email'
+        type='text'
+        placeholder='Email' />
+
+    </label>
+    <label>
+    <Field name='password'
+        type='text'
+        placeholder='Password' />
+    </label>
+    <input type='checkbox'
+    /> Terms of Service
+</div>
+<button type='submit'>Submit</button>
+</Form>
+
 export default Form;
